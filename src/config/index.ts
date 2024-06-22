2d22c8b2ee5ae0373e3f6a17a95b21fbc07a59fc
henrique <henrique.florencio307@gmail.com>
2024-06-22 12:14:26 -0300

    consertando vulnerabilidade

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,8 @@ configDotenv()
 
 export const config = {
     app: {
-        MONGODB_PASS: "9aKqU9MKv7hamDXi",
-        PORT: 3000,
-        JWT_SECRET: "YTDHxtSQf7rzgV9FvpuRM6Fc36TZkySNnHvpmbw4WPYDVkEfQrw65Uhj7tKJs2HGynH4Mzqb75fWeSU6Ndpx2tXsyr2sb7qvDjPn5g9MZQSWxH"
+        MONGODB_PASS: process.env.MONGODB_PASS,
+        PORT: process.env.PORT,
+        JWT_SECRET: process.env.JWT_SECRET
     }
 }
